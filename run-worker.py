96ec36f23e855898ae889fb5edf8062c6ff52a20
Tochi Ufomba <[tochiufomba@gmail.com]>
2025-01-15 17:41:40 -0600

    Fix Worker Process #2

--- run-worker.py
+++ run-worker.py
@@ -1,11 +1,11 @@
 import os
 import redis
-from redis import Redis, Connection
+from redis import Redis
 from rq import Queue
 from rq.worker import HerokuWorker as Worker
 
 
-listen = ['high', 'default', 'low']
+#listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDIS_URL')
 if not redis_url:
@@ -14,7 +14,7 @@ if not redis_url:
 
 conn = redis.from_url(redis_url)
 
-if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+conn.connect()
+worker = Worker([q], connection=conn)
+worker.work()
+conn.close()
